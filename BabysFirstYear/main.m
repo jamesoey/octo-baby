d6a8d7594b14cdf8e54b0e656b276948c4012925
James Oey <joey@shutterfly.com>
2013-05-16 18:50:42 -0700

    Initial Commit

---
+++ BabysFirstYear/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BabysFirstYear
+//
+//  Created by James Oey on 5/16/13.
+//  Copyright (c) 2013 Shutterfly, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
