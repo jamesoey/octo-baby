a36c8cfca0b3f5a82bb7be6e9485e871a6c360b9
James Oey <joey@shutterfly.com>
2013-05-17 18:13:09 -0700

    validating onboarding form

--- BabysFirstYear/Views/OnboardingView.m
+++ BabysFirstYear/Views/OnboardingView.m
@@ -25,45 +25,66 @@
     //top line
     //E0DBD7
     //FFFFFF
+    CGRect rect = CGRectMake(0, 0, 280, 2);
 
-/*           CGRect rect = CGRectMake(0, 0, 2, 635);
-           // Create a 1 pixel line
-           UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-           [color setFill];
-           UIRectFill(rect);   // Fill it with your color
-           UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-           UIGraphicsEndImageContext();
-           return image;
-    } */
+    UIColor *color0 = UIColorFromRGB(0xE0DBD7);
+    CGRect rect0 = CGRectMake(0, 0, 280, 1);
+
+    UIColor *color1 = UIColorFromRGB(0xFFFFFF);
+    CGRect rect1 = CGRectMake(0, 1, 280, 1);
+
+    // Create a 1 pixel line
+    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+    [color0 setFill];
+    UIRectFill(rect0); // Fill it with your color
+    [color1 setFill];
+    UIRectFill(rect1); // Fill it with your color
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return image;
+    
+}
+
+- (void)addLineAtY:(CGFloat)y {
+    UIImage *dLine = [self doubleLine];
+    UIImageView *iView = [[UIImageView alloc] initWithImage:dLine];
+    iView.frame = CGRectMake(20, y, dLine.size.width, dLine.size.height);
+    [self addSubview:iView];
 }
 
 - (void)initView {
     [self setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"imgIntroTile.png"]]];
     
+    [self addLineAtY:55];
+    [self addLineAtY:185];
+    [self addLineAtY:400];
+    
+    
     UIImage *shutterflyTitleImage = [UIImage imageNamed:@"imgSflyLogo.png"];
     UIImageView *shutterflyTitle = [[UIImageView alloc] initWithImage:shutterflyTitleImage];
-    [shutterflyTitle setFrame:CGRectMake((320-shutterflyTitleImage.size.width)/2.0, 0, shutterflyTitleImage.size.width, shutterflyTitleImage.size.height)];
+    [shutterflyTitle setFrame:CGRectMake((320-shutterflyTitleImage.size.width)/2.0, 20, shutterflyTitleImage.size.width, shutterflyTitleImage.size.height)];
     [self addSubview:shutterflyTitle];
     
     UIImage *introMessageImage = [UIImage imageNamed:@"imgIntroMessage.png"];
     UIImageView *introMessage = [[UIImageView alloc] initWithImage:introMessageImage];
-    [introMessage setFrame:CGRectMake((320-introMessageImage.size.width)/2.0, 50, introMessageImage.size.width, introMessageImage.size.height)];
+    [introMessage setFrame:CGRectMake((320-introMessageImage.size.width)/2.0, 80, introMessageImage.size.width, introMessageImage.size.height)];
     [self addSubview:introMessage];
     
-    self.nameField = [[UITextField alloc] initWithFrame:CGRectMake(20, 160, 280, 40)];
+    self.nameField = [[UITextField alloc] initWithFrame:CGRectMake(20, 220, 280, 30)];
     [self.nameField setBackgroundColor:[UIColor whiteColor]];
     self.nameField.placeholder = @"Baby's First Name";
     self.nameField.textColor = UIColorFromRGB(0x999999);
     [self addSubview: self.nameField];
 
-    self.dateField = [[UITextField alloc] initWithFrame:CGRectMake(20, 220, 280, 40)];
+    self.dateField = [[UITextField alloc] initWithFrame:CGRectMake(20, 270, 280, 30)];
     [self.dateField setBackgroundColor:[UIColor whiteColor]];
     self.dateField.placeholder = @"Baby's Birthdate";
     self.dateField.textColor = UIColorFromRGB(0x999999);
     [self addSubview: self.dateField];
 
     
-    CGFloat genderY = 300;
+    CGFloat genderY = 340;
     UIImage *genderDisabled = [UIImage imageNamed:@"imgGenderRadial.png"];
     UIImage *boyImage = [UIImage imageNamed:@"imgBoySelection.png"];
     UIImage *girlImage = [UIImage imageNamed:@"imgGirlSelection.png"];
@@ -71,32 +92,33 @@
     self.girlButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.girlButton setImage:genderDisabled forState:UIControlStateNormal];
     [self.girlButton setImage:girlImage forState:UIControlStateSelected];
-    [self.girlButton setFrame:CGRectMake(100, genderY, genderDisabled.size.width, genderDisabled.size.height)];
+    [self.girlButton setFrame:CGRectMake(60, genderY, genderDisabled.size.width, genderDisabled.size.height)];
     [self addSubview:self.girlButton];
     
-    UILabel *girlText = [[UILabel alloc] initWithFrame:CGRectMake(140, genderY, 80, 40)];
+    UILabel *girlText = [[UILabel alloc] initWithFrame:CGRectMake(110, genderY, 80, 40)];
     girlText.text = @"Girl";
     girlText.backgroundColor = [UIColor clearColor];
     girlText.textColor = UIColorFromRGB(0x666666);
     [self addSubview:girlText];
     
-    UILabel *boyText = [[UILabel alloc] initWithFrame:CGRectMake(260, genderY, 80, 40)];
+    self.boyButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.boyButton setImage:genderDisabled forState:UIControlStateNormal];
+    [self.boyButton setImage:boyImage forState:UIControlStateSelected];
+    [self.boyButton setFrame:CGRectMake(170, genderY, genderDisabled.size.width, genderDisabled.size.height)];
+    [self addSubview:self.boyButton];
+    
+    UILabel *boyText = [[UILabel alloc] initWithFrame:CGRectMake(220, genderY, 80, 40)];
     boyText.text = @"Boy";
     boyText.backgroundColor = [UIColor clearColor];
     boyText.textColor = UIColorFromRGB(0x666666);
     [self addSubview:boyText];
     
-    self.boyButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.boyButton setImage:genderDisabled forState:UIControlStateNormal];
-    [self.boyButton setImage:boyImage forState:UIControlStateSelected];
-    [self.boyButton setFrame:CGRectMake(200, genderY, genderDisabled.size.width, genderDisabled.size.height)];
-    [self addSubview:self.boyButton];
-    
     
     UIImage *btnBeginImage = [UIImage imageNamed:@"btnBegin"];
     self.submitButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [self.submitButton setImage:btnBeginImage forState:UIControlStateNormal];
-    [self.submitButton setFrame:CGRectMake((320-btnBeginImage.size.width)/2.0, 400, btnBeginImage.size.width, btnBeginImage.size.height)];
+    [self.submitButton setFrame:CGRectMake((320-btnBeginImage.size.width)/2.0, 440, btnBeginImage.size.width, btnBeginImage.size.height)];
+    self.submitButton.alpha = 0.4;
     [self addSubview:self.submitButton];
     
     self.birthdatePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, self.bounds.size.height-250, 320, 250)];
